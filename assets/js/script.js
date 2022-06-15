bb815c20f4b0033c320807e78815fc1e155f5635
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-06-15 08:23:21 -0300

    Adiciona outra condiciona: menor que 0 fica vermelhoo, se for maior, verde

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,46 @@
 var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0
 
+// function increment() {
+//     if (currentNumber < 10 && currentNumber >= 0) {
+//         currentNumber = currentNumber + 1
+//         currentNumberWrapper.innerHTML = currentNumber
+//     } else if (currentNumber === 10) {
+//         alert("Número máximo alcançado!")
+//     }
+// }
+
+// function decrement() {
+//     if (currentNumber <= 10 && currentNumber > 0) {
+//         currentNumber = currentNumber - 1
+//         currentNumberWrapper.innerHTML = currentNumber
+//     } else if (currentNumber === 0) {
+//         alert("Número mínimo alcançado!")
+//     }
+// }
+
 function increment() {
-    if (currentNumber < 10 && currentNumber >= 0) {
-        currentNumber = currentNumber + 1
-        currentNumberWrapper.innerHTML = currentNumber
-    } else if (currentNumber === 10) {
-        alert("Número máximo alcançado!")
+    currentNumber = currentNumber + 1
+    currentNumberWrapper.innerHTML = currentNumber
+    if (currentNumber >= 0) {
+        currentNumberWrapper.style.color = 'green'
+    } else {
+        currentNumberWrapper.style.color = 'red'
     }
 }
 
 function decrement() {
-    if (currentNumber <= 10 && currentNumber > 0) {
         currentNumber = currentNumber - 1
         currentNumberWrapper.innerHTML = currentNumber
-    } else if (currentNumber === 0) {
-        alert("Número mínimo alcançado!")
-    }
+        if (currentNumber >= 0) {
+            currentNumberWrapper.style.color = 'green'
+        } else {
+            currentNumberWrapper.style.color = 'red'
+        }
 }
 
+if (currentNumber >= 0) {
+        currentNumberWrapper.style.color = 'green'
+    } else {
+        currentNumberWrapper.style.color = 'red'
+    }
