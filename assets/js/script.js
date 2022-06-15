cc73f70047732ac780901eb12bc1d9379fdfd34c
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-06-15 08:16:52 -0300

    Adiciona condicional: limite de 10 e mínimo de 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,20 @@ var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0
 
 function increment() {
-    currentNumber = currentNumber + 1
-    currentNumberWrapper.innerHTML = currentNumber
+    if (currentNumber < 10 && currentNumber >= 0) {
+        currentNumber = currentNumber + 1
+        currentNumberWrapper.innerHTML = currentNumber
+    } else if (currentNumber === 10) {
+        alert("Número máximo alcançado!")
+    }
 }
 
 function decrement() {
-    currentNumber = currentNumber - 1
-    currentNumberWrapper.innerHTML = currentNumber
+    if (currentNumber <= 10 && currentNumber > 0) {
+        currentNumber = currentNumber - 1
+        currentNumberWrapper.innerHTML = currentNumber
+    } else if (currentNumber === 0) {
+        alert("Número mínimo alcançado!")
+    }
 }
+
